2306ef5317e36f97b4d7bd42934c40be061711f9
liangjies <1139629972@qq.com>
2022-03-13 23:43:05 +0800

    commit

--- global/model.go
+++ global/model.go
@@ -8,7 +8,7 @@ import (
 
 type SYS_MODEL struct {
 	ID        uint           `json:"id" gorm:"primarykey"` // 主键ID
-	CreatedAt time.Time      // 创建时间
-	UpdatedAt time.Time      // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
+	CreatedAt time.Time      `json:"createdAt"`            // 创建时间
+	UpdatedAt time.Time      `json:"updatedAt"`            // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
 }
