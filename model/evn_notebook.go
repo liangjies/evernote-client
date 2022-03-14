a66df3db69b95645be3bc4e75859ad7a62b5d30d
liangjies <1139629972@qq.com>
2022-03-15 00:11:20 +0800

    commit

--- model/evn_notebook.go
+++ model/evn_notebook.go
@@ -7,6 +7,6 @@ import (
 type EvnNotebook struct {
 	global.SYS_MODEL
 	Title      string `json:"title" gorm:"size:50;column:title;comment:笔记本标题"`
-	CreateBy   uint   `json:"createBy" gorm:"index;column:create_by;comment:创建人"`
+	CreateBy   uint   `json:"_" gorm:"index;column:create_by;comment:创建人"`
 	NoteCounts uint   `json:"noteCounts" gorm:"column:note_counts;comment:笔记数量"`
 }
